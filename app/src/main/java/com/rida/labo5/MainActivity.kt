261354778382606c99b455537020d7d26c11edf1
ridamellouki3 <ridamellouki123@gmail.com>
2024-12-02 10:51:38 +0100

    Initial commit

---
+++ app/src/main/java/com/rida/labo5/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.rida.labo5
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        //supportActionBar?.hide()
+        setContentView(R.layout.activity_main)
+
+    }
+}
